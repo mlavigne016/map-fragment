6cc083b43bb431a6f2d37fd229fbbf2dd606f7d9
Miguel Lavigne <mlavigne@lixar.com>
2012-11-09 19:06:53 -0500

    Attempt to fix some of the MapView memory issues
    MapView has some static fields that seems to cause memory issues.
    We're now attempting to clear them manually using reflexion before
    unloading the MapView.

--- library/src/com/mapfragment/library/MapFragment.java
+++ library/src/com/mapfragment/library/MapFragment.java
@@ -1,8 +1,13 @@
 package com.mapfragment.library;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import com.google.android.maps.MapView;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -37,6 +42,100 @@ public class MapFragment extends ActivityHostFragment {
 	@Override
 	public void onDestroyView() {
 		super.onDestroyView();
+		cleanUpMapView();
+		releaseMapView();
+	}
+
+	private void cleanUpMapView() {
+		cleanUpMapViewField();
+		cleanUpMapViewController();
+		cleanUpMapViewConverter();
+		cleanUpMapViewZoomHelper();
+		cleanUpMapViewOverlays();
+	}
+
+	private void cleanUpMapViewField() {
+		try {
+			releaseMapViewMapField();
+		} catch (final NoSuchFieldException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewField() failed", e);
+		} catch (final IllegalAccessException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewField() failed", e);
+		}
+	}
+
+	private void cleanUpMapViewController() {
+		try {
+			releaseMapViewController();
+		} catch (final NoSuchFieldException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewController() failed", e);
+		} catch (final IllegalAccessException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewController() failed", e);
+		}
+	}
+
+	private void cleanUpMapViewConverter() {
+		try {
+			releaseMapViewConverter();
+		} catch (final NoSuchFieldException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewConverter() failed", e);
+		} catch (final IllegalAccessException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewConverter() failed", e);
+		}
+	}
+
+	private void cleanUpMapViewZoomHelper() {
+		try {
+			releaseMapViewZoomHelper();
+		} catch (final NoSuchFieldException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewZoomHelper() failed", e);
+		} catch (final IllegalAccessException e) {
+			Log.e(MapFragment.class.getSimpleName(), "cleanUpMapViewZoomHelper() failed", e);
+		}
+	}
+
+	private void cleanUpMapViewOverlays() {
+		releaseMapViewOverlays();
+	}
+
+	private void releaseMapViewMapField() throws NoSuchFieldException, IllegalAccessException {
+		Class<?> MapClass = mapView.getClass();
+		Field fMapInView = MapClass.getDeclaredField("mMap");
+		AccessibleObject.setAccessible(new AccessibleObject[]{fMapInView}, true);
+		fMapInView.set(mapView, null);
+	}
+
+	private void releaseMapViewConverter() throws NoSuchFieldException, IllegalAccessException {
+		Field fConverterInView = MapView.class.getDeclaredField("mConverter");
+		AccessibleObject.setAccessible(new AccessibleObject[]{fConverterInView}, true);
+		fConverterInView.set(mapView, null);
+	}
+
+	private void releaseMapViewController() throws NoSuchFieldException, IllegalAccessException {
+		Field fControllerInView = MapView.class.getDeclaredField("mController");
+		AccessibleObject.setAccessible(new AccessibleObject[]{fControllerInView}, true);
+		fControllerInView.set(mapView, null);
+	}
+
+	private void releaseMapViewZoomHelper() throws NoSuchFieldException, IllegalAccessException {
+		Field fZoomHelperInView = MapView.class.getDeclaredField("mZoomHelper");
+		AccessibleObject.setAccessible(new AccessibleObject[]{fZoomHelperInView}, true);
+		fZoomHelperInView.set(mapView, null);
+	}
+
+	private void releaseMapViewOverlays() {
+		int size = mapView.getOverlays().size();
+		for (int i = size; i > 0; i--) {
+			mapView.getOverlays().remove(i - 1);
+		}
+	}
+
+	private void releaseMapView() {
+		ViewParent viewParent = mapView.getParent();
+		if (viewParent != null) {
+			ViewGroup viewGroup = (ViewGroup) viewParent;
+			viewGroup.removeView(mapView);
+		}
 		mapView = null;
 	}
 }
